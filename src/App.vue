71a7316623fa023855ac59ed5d812acecde09a3a
jugiros <31550758+jugiros@users.noreply.github.com>
2023-06-25 21:37:32 -0500

    Actualización para validar si el usuario existe y verificar las acciones a validar.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
-  <router-view/>
+  <template v-if="user">
+    <router-view />
+  </template>
+  <div v-if="!user && user !== undefined">
+    <p>Zona login/registro</p>
+  </div>
 </template>
 
 <script>
-import {onMounted} from "vue";
-import { auth } from './utils/firebase.js';
+import { onMounted, computed } from "vue";
+import { auth } from "./utils/firebase";
+import { useStore } from "vuex";
+
 export default {
   setup() {
+    const store = useStore();
+    const user = computed(() => store.state.user);
+
     onMounted(() => {
       auth.onAuthStateChanged((user) => {
         console.log(user);
-      })
-    })
-  }
+        store.commit("setUser", user);
+      });
+    });
+    return {
+      user,
+    };
+  },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,15 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    user: undefined,
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    setUser(state, payload) {
+      state.user = payload;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
