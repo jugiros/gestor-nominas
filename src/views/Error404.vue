b6322c80a2ed020a2fd281676071286e8f186064
jugiros <31550758+jugiros@users.noreply.github.com>
2023-06-26 01:08:08 -0500

    Actualización diseño de la página de error.

--- src/views/Error404.vue
+++ src/views/Error404.vue
@@ -1,6 +1,11 @@
 <template>
-  <h1>Error404</h1>
-  <div class="return-home"><router-link to="/">IR AL INICIO</router-link></div>
+  <div class="ui container">
+    <div class="ui center">
+      <img src="../assets/nomina.png">
+      <h1>Error404</h1>
+      <div class="return-home"><router-link to="/">IR AL INICIO</router-link></div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,14 +15,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-h1 {
-  color: #e22e2e;
-  font-size: 50px;
-  font-weight: bold;
-  text-align: center;
-  margin-top: 20%;
-}
-.return-home {
-  text-align: center;
+.ui.container {
+  .ui.center {
+    text-align: center;
+  }
+  h1 {
+    color: #e22e2e;
+    font-size: 50px;
+    font-weight: bold;
+    text-align: center;
+  }
+  a {
+    color: #16202b;
+    font-weight: bold;
+  }
+  img {
+    margin-top: 20%;
+    width: 40px;
+  }
+  .return-home {
+    text-align: center;
+  }
 }
 </style>
