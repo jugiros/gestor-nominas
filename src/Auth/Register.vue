080fcf346903c3f1d1c9080c62a768a7e5365d03
jugiros <31550758+jugiros@users.noreply.github.com>
2023-06-25 22:24:46 -0500

    Creación y validación del formulario de registro con validación de estilos scss.

--- src/Auth/Register.vue
+++ src/Auth/Register.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="register">
-    <h2>Register</h2>
-    <button @click="changeForm">Iniciar sesión</button>
+    <h1>Crear Cuenta</h1>
+    <form class="ui form">
+      <div style="width: 100%" class="ui input field">
+        <input type="text" placeholder="Correo electrónico" />
+      </div>
+      <div style="width: 100%" class="ui input field">
+        <input type="text" placeholder="Contraseña" />
+      </div>
+      <div style="width: 100%" class="ui input field">
+        <input type="text" placeholder="Repetir contraseña" />
+      </div>
+      <br />
+      <button class="ui primary button" type="submit">Registrar</button>
+      <p @click="changeForm">Iniciar sesión</p>
+    </form>
   </div>
 </template>
 
@@ -16,4 +29,37 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.register {
+  background-color: #fff;
+  padding: 30px;
+  box-shadow: 0px 0px 38px -5px rgba(0, 0, 0, 0.45);
+  width: 400px;
+  border-radius: 10px;
+  h1 {
+    text-align: center;
+    margin-bottom: 30px;
+  }
+  field {
+    width: 100% !important;
+    margin-bottom: 10px;
+  }
+  form {
+    button {
+      width: 100%;
+    }
+    input.error {
+      border-color: #faa;
+      background-color: #ffeded;
+    }
+    p {
+      text-align: center;
+      margin-top: 30px;
+      &:hover {
+        cursor: pointer;
+        opacity: 0.6;
+      }
+    }
+  }
+}
+</style>
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="auth">
-    <h1>Estamos en AUTH</h1>
     <login v-if="showLogin" :change-form="chageForm"></login>
     <register v-else :change-form="chageForm"></register>
   </div>
@@ -15,7 +14,7 @@ export default {
   name: "Auth",
   components: { Register, Login },
   setup() {
-    let showLogin = ref(true);
+    let showLogin = ref(false);
 
     const chageForm = () => {
       showLogin.value = !showLogin.value;
@@ -23,10 +22,24 @@ export default {
 
     return {
       showLogin,
-      chageForm
+      chageForm,
     };
   },
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.auth {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: rgb(4, 78, 157);
+  background: linear-gradient(
+    0deg,
+    rgba(4, 78, 157, 1) 0%,
+    rgba(0, 174, 255, 1) 100%
+  );
+}
+</style>
